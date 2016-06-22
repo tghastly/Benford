980f8296d2e02769889bcf3f59394d0b11560e36
tghastly <tghastly@users.noreply.github.com>
2016-06-21 20:43:51 -0700

    Updated author to T. Ghastly

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,8 +48,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Benford\'s Law'
-copyright = '2016, P. Lysaght'
-author = 'P. Lysaght'
+copyright = '2016, T. Ghastly'
+author = 'T. Ghastly'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -225,7 +225,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'BenfordsLaw.tex', 'Benford\'s Law Documentation',
-   'P. Lysaght', 'manual'),
+   'T. Ghastly', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
